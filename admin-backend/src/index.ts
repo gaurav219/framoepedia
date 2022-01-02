213679c45cf268739ee3744d6d0a8113178bdafa
Gaurav Cherwal <gauravcherwal@outlook.com>
2022-01-02 20:01:00 +0530

    updated cors changes

--- admin-backend/src/index.ts
+++ admin-backend/src/index.ts
@@ -1,16 +1,25 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 const app: Express = express();
+import cors from "cors";
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello from admin-backend</h1>');
+const allowedOrigins = ["http://localhost:3000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Hello from admin-backend</h1>");
 });
 
-app.get('/home', (req: Request, res: Response) => {
-  res.json({ name: 'gopuuuuuuu' });
+app.get("/home", (req: Request, res: Response) => {
+  res.json({ name: "gopuuuuuuu" });
 });
 
 app.listen(5000, () => {
-  console.log('Listening to port 5000...');
+  console.log("Listening to port 5000...");
 });
--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const func = async () => {
-    const res = await fetch('http://localhost:5000/home');
-    console.log(res);
+    const res = await fetch("http://localhost:5000/home");
+    const response = await res.json();
+    console.log(response);
   };
 
   useEffect(() => {
@@ -13,17 +14,17 @@ function App() {
   }, []);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Hello from admin-frontend
         </a>
