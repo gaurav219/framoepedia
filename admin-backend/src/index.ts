605adad1639c0b6c0d05136bb7e3ec1a4f0b7c21
Gaurav Cherwal <gauravcherwal@outlook.com>
2022-01-03 21:33:17 +0530

    added graphql to admin-backend

--- admin-backend/src/index.ts
+++ admin-backend/src/index.ts
@@ -1,25 +1,40 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
 import cors from "cors";
-
+import { ApolloServer, gql } from "apollo-server-express";
+import { typeDef as typeDefs, resolvers } from "./schema/image";
 app.use(express.json());
 
 const allowedOrigins = ["http://localhost:3000"];
 
-const options: cors.CorsOptions = {
-  origin: allowedOrigins,
+const init = async () => {
+  const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+  };
+
+  const server: ApolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+  });
+
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use(cors(options));
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("<h1>Hello from admin-backend</h1>");
+  });
+
+  app.get("/home", (req: Request, res: Response) => {
+    res.json({ name: "gopuuuuuuu" });
+  });
+
+  app.listen(5000, () => {
+    console.log("Listening to port 5000... and graphql at /graphql");
+  });
 };
 
-app.use(cors(options));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hello from admin-backend</h1>");
-});
-
-app.get("/home", (req: Request, res: Response) => {
-  res.json({ name: "gopuuuuuuu" });
-});
-
-app.listen(5000, () => {
-  console.log("Listening to port 5000...");
-});
+init();
