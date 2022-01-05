445ca6498cf6839a12d8be736ea194830d8f48a0
pineapple45 <gomraanmol@gmail.com>
2022-01-05 13:52:28 +0530

    adding postgres

---
+++ storage-broker/src/index.ts
@@ -0,0 +1,19 @@
+// Require the framework and instantiate it
+
+// ESM
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
+
+// Run the server!
+fastify.listen(5005, (err, address) => {
+  if (err) throw err;
+  // Server is now listening on ${address}
+  console.log('Storage broker listening on PORT 5005..');
+});
