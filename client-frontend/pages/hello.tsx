99db8f198f2a0e8a74d42d6fc121848d4e421ef6
Gaurav Cherwal <gauravcherwal@outlook.com>
2022-01-09 21:00:32 +0530

    synced with graphql changes

--- client-frontend/pages/hello.tsx
+++ client-frontend/pages/hello.tsx
@@ -3,14 +3,23 @@ import { useState } from "react";
 import { ApolloClient, gql, InMemoryCache, useQuery } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
+import Image from "next/image";
 // import Axios from "axios";
 import { Images } from "./interfaces/Image";
 import getImages from "./queries/image";
 import styles from "../styles/Home.module.css";
 import client from "../apollo-client";
-// type Data = {
-//   name: string;
-// };
+import favicon from "../public/favicon.ico";
+
+type Data = {
+  _: Image[];
+};
+
+interface Image {
+  id: String;
+  url: String;
+  title: String;
+}
 
 // function Home({ hello }: InferGetStaticPropsType<typeof getStaticProps>) {
 const Home: NextPage = ({
@@ -20,12 +29,24 @@ const Home: NextPage = ({
   //   console.log(data.data)
   // );
   // console.log(error, "er");
-  console.log(data);
+  // console.log(data);
+
+  data.images.forEach((image: Image) => console.log(image.id));
   // fetch("http://localhost:5001/home")
   //   .then((res) => res.json())
   //   .then((data) => console.log(data));
 
-  return <div className={styles.container}>Smpl</div>;
+  return (
+    <>
+      <div className={styles.container}>Smpl</div>
+      <Image
+        src={favicon}
+        alt="AltPicture of Next"
+        priority
+        // layout="responsive"
+      />
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -35,6 +56,16 @@ export const getStaticProps: GetStaticProps = async (context) => {
   //   uri: "http://127.0.0.1:5001/graphql",
   //   cache: new InMemoryCache(),
   // });
+  const res = await fetch(
+    `http://localhost:3000/api/${context.preview ? "preview" : ""}`
+  );
+
+  // console.log(await res.json(), "json");
+
+  console.log(context.preview, "preview");
+
+  console.log(context.previewData);
+
   const { data } = await client.query({
     query: getImages,
   });
